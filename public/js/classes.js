3e72019393acc9d7ea95857afbdc955c0ed9c145
gcomartins <gcomartins@gmail.com>
2021-12-16 17:19:29 -0300

    Desenvolvimento da visualização web

--- public/js/classes.js
+++ public/js/classes.js
@@ -13,16 +13,21 @@ class Personagem {
         if (adversario.vida <= 0) {
 
             console.log(`${adversario.nome} já está inconsciente!`);
+            historico.innerHTML += `<br>${adversario.nome} já está inconsciente!`
 
         } else {
             
             console.log(`${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade1.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade1.multiplicador} de dano`); 
+
+            historico.innerHTML += `<br>${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade1.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade1.multiplicador} de dano`
     
             adversario.defender1(this);
     
             console.log(`Vida ${this.nome}: ${this.vida}`);
+            historico.innerHTML += `<br>Vida ${this.nome}: ${this.vida}`
     
             console.log(`Vida ${adversario.nome}: ${adversario.vida}`);
+            historico.innerHTML += `<br>Vida ${adversario.nome}: ${adversario.vida}`
 
         }
 
@@ -30,16 +35,22 @@ class Personagem {
     atacar2(adversario){
 
         if (adversario.vida <= 0) {
+
             console.log(`${adversario.nome} já está insconsciente!`);
+            historico.innerHTML += `<br>${adversario.nome} já está insconsciente!`
+
         } else {
             
             console.log(`${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade2.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade2.multiplicador} de dano`); 
+            historico.innerHTML += `<br>${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade2.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade2.multiplicador} de dano`
     
             adversario.defender2(this);
     
             console.log(`Vida ${this.nome}: ${this.vida}`);
-    
+            historico.innerHTML += `<br>Vida ${this.nome}: ${this.vida}`
+
             console.log(`Vida ${adversario.nome}: ${adversario.vida}`);
+            historico.innerHTML += `<br>Vida ${adversario.nome}: ${adversario.vida}`
                 
         }
     }
@@ -49,17 +60,19 @@ class Personagem {
         let dano = adversario.equipamento.arma.dano*adversario.equipamento.arma.habilidades.habilidade1.multiplicador;
         let defesa = this.equipamento.peito.armadura + this.equipamento.pernas.armadura + this.equipamento.pes.armadura;
 
-        if (Math.random()>0.2) {
+        if (Math.random() > 0.2) {
 
-            this.vida -= (dano-defesa);
+            this.vida -= dano-defesa;
 
             console.log(`${this.nome} defendeu o ataque de ${adversario.nome} e tomou ${dano - defesa} de dano de ${adversario.nome}`);
+            historico.innerHTML += `<br>${this.nome} defendeu o ataque de ${adversario.nome} e tomou ${dano - defesa} de dano de ${adversario.nome}`
 
         } else {
 
-            this.vida -= (dano);
+            this.vida -= dano;
 
             console.log(`${this.nome} tomou ${dano} de dano de ${adversario.nome}`);
+            historico.innerHTML += `<br>${this.nome} tomou ${dano} de dano de ${adversario.nome}`
 
         }
     }
@@ -68,15 +81,19 @@ class Personagem {
         let dano = adversario.equipamento.arma.dano*adversario.equipamento.arma.habilidades.habilidade2.multiplicador;
         let defesa = this.equipamento.peito.armadura + this.equipamento.pernas.armadura + this.equipamento.pes.armadura;
 
-        if (Math.random()>0.2) {
-            this.vida -= (dano-defesa);
+        if (Math.random() > 0.2) {
 
-            console.log(`${this.nome} defendeu o ataque de ${adversario.nome} e tomou ${dano - defesa} de dano de ${adversario.nome}`); 
+            this.vida -= dano-defesa;
+
+            console.log(`${this.nome} defendeu o ataque de ${adversario.nome} e tomou ${dano - defesa} de dano de ${adversario.nome}`);
+            historico.innerHTML += `<br>${this.nome} defendeu o ataque de ${adversario.nome} e tomou ${dano - defesa} de dano de ${adversario.nome}` 
             
         } else {
-            this.vida -= (dano);
+
+            this.vida -= dano;
             
-            console.log(`${this.nome} tomou ${dano} de dano de ${adversario.nome}`); 
+            console.log(`${this.nome} tomou ${dano} de dano de ${adversario.nome}`);
+            historico.innerHTML += `<br>${this.nome} tomou ${dano} de dano de ${adversario.nome}` 
 
         }
     }
