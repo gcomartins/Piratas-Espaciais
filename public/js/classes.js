cacf6a85f4b064ed5be858118a64822856a36bb5
gcomartins <gcomartins@gmail.com>
2021-12-16 11:52:20 -0300

    Adicionei vida aos personagens, ajustei a disposição das passivas e fiz funções pras 2 habilidades

--- public/js/classes.js
+++ public/js/classes.js
@@ -1,20 +1,24 @@
 class Personagem {
-    constructor(nome, idade, classe, equipamento, habilidades){
+    constructor(vida, nome, idade, classe, equipamento, passiva){
+        this.vida = vida
         this.nome= nome;
         this.classe = classe;
         this.idade= idade;
         this.equipamento= equipamento;
-        this.habilidades= habilidades;
+        this.passiva= passiva;
     }
 
-    atacar(adversario){
-        return `${this.nome} atacou ${adversario.nome}`
+    atacar1(adversario){
+        return `${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade1.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade1.multiplicador} de dano`
+    }
+    atacar2(adversario){
+        return `${this.nome} atacou ${adversario.nome} com ${this.equipamento.arma.habilidades.habilidade2.nome} e pode causar ${this.equipamento.arma.dano*this.equipamento.arma.habilidades.habilidade2.multiplicador} de dano`
     }
     defender(adversario){
-        return `${this.nome} defendeu ${adversario.nome}`
+        return `${this.nome} defendeu o ataque de ${adversario.nome}`
     }
     levar(adversario){
-        return `${this.nome} tomou 10 de dano de ${adversario.nome}`
+        return `${this.nome} tomou ${adversario.equipamento.arma.dano} de dano de ${adversario.nome}`
     }
 }
 class Equipamento {
